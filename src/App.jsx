baac2ca6e5835ee83308e1eefcadd5acf3b12e36
Pedro Henrique <pedro.semear@gmail.com>
2024-01-02 17:27:05 -0300

    create project vite react+ts

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Post } from "./Post";
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <Post author="Pedroca" content="anblasbdkhsfdasdf.,nfvgjadnfggakggrjgk" />
+      <Post author="Joao" content="adbauyfgcbnegfuywcebdanbfv,fdbgjabgvkabgg" />
+    </>
+  );
+}
+
+export default App;
