54f1a4b1ebcf9fa5a50d4b492038627860b0b40d
Pedro Henrique <pedro.semear@gmail.com>
2024-01-29 14:28:24 -0300

    change app design

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header/Header";
 import './global.css'
+import styles from './App.module.css'
 export function App() {
   return (
     <>
       <Header/>
-      <Post author="Pedroca" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officia incidunt labore nobis asperiores fugit nemo accusamus ipsam? Nostrum id molestias atque aut. Inventore veniam perspiciatis ea eveniet autem vero." />
-      <Post author="Joao" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officia incidunt labore nobis asperiores fugit nemo accusamus ipsam? Nostrum id molestias atque aut. Inventore veniam perspiciatis ea eveniet autem vero." />
+      <div className={styles.wrapper}>
+        <aside>
+          sidebar
+        </aside>
+        <main>
+          <Post author="Pedroca" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officia incidunt labore nobis asperiores fugit nemo accusamus ipsam? Nostrum id molestias atque aut. Inventore veniam perspiciatis ea eveniet autem vero." />
+          <Post author="Joao" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officia incidunt labore nobis asperiores fugit nemo accusamus ipsam? Nostrum id molestias atque aut. Inventore veniam perspiciatis ea eveniet autem vero." />
+        </main>
+      </div>
     </>
   );
 }
