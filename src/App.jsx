8bc2a8b2201a0cb918d26109bd4e27940b0e1669
Pedro Henrique <pedro.semear@gmail.com>
2024-01-29 01:57:26 -0300

    create Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Post } from "./Post";
-function App() {
+import { Header } from "./components/Header/Header";
+import './global.css'
+export function App() {
   return (
     <>
-      <h1>Vite + React</h1>
-      <Post author="Pedroca" content="anblasbdkhsfdasdf.,nfvgjadnfggakggrjgk" />
-      <Post author="Joao" content="adbauyfgcbnegfuywcebdanbfv,fdbgjabgvkabgg" />
+      <Header/>
+      <Post author="Pedroca" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officia incidunt labore nobis asperiores fugit nemo accusamus ipsam? Nostrum id molestias atque aut. Inventore veniam perspiciatis ea eveniet autem vero." />
+      <Post author="Joao" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officia incidunt labore nobis asperiores fugit nemo accusamus ipsam? Nostrum id molestias atque aut. Inventore veniam perspiciatis ea eveniet autem vero." />
     </>
   );
 }
 
-export default App;
