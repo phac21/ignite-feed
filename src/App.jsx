c8572f64309da442b4c8c8e1a30646455b47200d
Pedro Henrique <pedro.semear@gmail.com>
2024-02-02 00:30:58 -0300

    create/ sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { Post } from "./Post";
 import { Header } from "./components/Header/Header";
 import './global.css'
 import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar/Sidebar";
 export function App() {
   return (
     <>
       <Header/>
       <div className={styles.wrapper}>
-        <aside>
-          sidebar
-        </aside>
+        <Sidebar/>
         <main>
           <Post author="Pedroca" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officia incidunt labore nobis asperiores fugit nemo accusamus ipsam? Nostrum id molestias atque aut. Inventore veniam perspiciatis ea eveniet autem vero." />
           <Post author="Joao" content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur officia incidunt labore nobis asperiores fugit nemo accusamus ipsam? Nostrum id molestias atque aut. Inventore veniam perspiciatis ea eveniet autem vero." />
---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,18 @@
+import styles from "./Sidebar.module.css"
+
+export function Sidebar() {
+    return(
+       <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1526779259212-939e64788e3c?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            <div className={styles.profile}>
+                <strong>Pedro Henrique</strong>
+                <span>Software Enginieer</span>
+            </div>
+            <footer>
+                <a href="#"> 
+                    Editar seu perfil
+                </a>
+            </footer>
+       </aside>
+    )
+}
